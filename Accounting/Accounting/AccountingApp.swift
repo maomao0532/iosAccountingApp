bdcb7aba3ac4248b7fce03fdf685bc8939635695
maomao0532 <1565823755@qq.com>
2026-04-27 00:15:42 +0800

    project build

---
+++ Accounting/Accounting/AccountingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountingApp.swift
+//  Accounting
+//
+//  Created by LiuDC on 2026/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AccountingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accounting/Accounting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Accounting
+//
+//  Created by LiuDC on 2026/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
